d8542be0749d46fa5d821d2b61c3b79c6d89105b
maksymstepaniuk <107501844+maksymstepaniuk@users.noreply.github.com>
2023-01-18 15:07:14 +0200

    Add files via upload

---
+++ tesla/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('.slider').slick({
+		arrows: false,
+		fade: true,
+		autoplay: 3000,
+		dots: true
+	});
+
+	$('.header__btn').on('click', function(){
+		$('.menu').addClass('active');
+	});
+	$('.close-btn').on('click', function(){
+		$('.menu').removeClass('active');
+	});
+});
